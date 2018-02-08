064ce0642946f24f18c97fabd6b7f7b7b5da6c29
Dusan Rychnovsky <durychno@microsoft.com>
2018-02-08 18:36:40 +0100

    Add JUnit.

---
+++ src/test/java/cz/dusanrychnovsky/booleanexpressions/BooleanExpressionsTest.java
@@ -0,0 +1,14 @@
+package cz.dusanrychnovsky.booleanexpressions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BooleanExpressionsTest {
+
+  @Test
+  public void canEvaluateSimpleExpression()
+  {
+    System.out.println("Hello world!");
+    Assert.fail();
+  }
+}
